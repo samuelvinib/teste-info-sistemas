5571e4aff0c820a853a8afa2b4e399926c64b21c
Samuel Vinícius <samuelvinib@gmail.com>
2023-06-06 02:13:02 -0300

    CHORE: botão para deletar carro adicionado

--- app/src/app/components/card/card.component.ts
+++ app/src/app/components/card/card.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { PlacaPipe } from 'src/app/pipes/placa';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-card',
@@ -8,11 +11,36 @@ import { PlacaPipe } from 'src/app/pipes/placa';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
   public apiEndpoint:string = environment.apiUrl;
   @Input() car!: any;
-  constructor()  { }
+  
+  constructor(private dialog: MatDialog, private http: HttpClient, private toastr: ToastrService)  { }
 
   ngOnInit(): void {
   }
 
+  openConfirmationPopup(carId: number): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '400px',
+      data: { carId: carId }
+    });
+  dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.http.delete(`${environment.apiUrl}/${carId}`)
+        .subscribe(
+          (response) => {
+            this.toastr.success('Carro excluido com sucesso!','Sucesso');
+            setTimeout(()=>{
+              window.location.reload();
+            },1000)
+          },
+          (error) => {
+            console.error('Erro ao criar o carro:', error);
+            this.toastr.error('Insira todos os dados e uma imagem para criar um carro', 'Erro');
+          }
+        );
+      }
+    });
+  }
 }
